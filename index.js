52b563bb9bfef6a65c9bdfce50b60c2fc1d679f1
Shubham Dhangar <shubhdhangar7067@gmail.com>
2023-07-20 18:29:40 +0530

    five

--- index.js
+++ index.js
@@ -6,19 +6,8 @@ const cors = require('cors');
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:3001', 'http://example.com'];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
 
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 
